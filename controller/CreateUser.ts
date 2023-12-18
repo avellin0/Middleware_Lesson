69e4f30390706e8c81d93501bdcab947070d89a3
Ðåvï åvêlïñð <98717652+avellin0@users.noreply.github.com>
2023-12-18 09:16:37 -0300

    Update CreateUser.ts

--- controller/CreateUser.ts
+++ controller/CreateUser.ts
@@ -5,16 +5,13 @@ export class GetUsers{
     async handle(req: Request, res:Response){  
     
     try{
-        await client.connect()
         console.log("Connected")
         const result = await client.query('SELECT * FROM employees')
-        console.table(result.rows);
         res.send(result.rows)
     }catch(e){
         console.log(`Something wrong happend here ${e}`)
     
     }finally{
-          client.end()
             console.log("Disconnected")
         }
 
