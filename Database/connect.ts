d2d82e9b840d2313d84d92a5b72b2f1ebb478ec2
Ðåvï åvêlïñð <98717652+avellin0@users.noreply.github.com>
2023-12-18 09:18:28 -0300

    Update connect.ts

--- Database/connect.ts
+++ Database/connect.ts
@@ -1,11 +1,12 @@
+import 'dotenv/config'
 import {Pool} from "pg"
 
 const client = new Pool({
-    user: "postgres",
-    host: "localhost",
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
     port: 5432,
-    database: "company",
-    password: "pico"
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS
 })
 
  
